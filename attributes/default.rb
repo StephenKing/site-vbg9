aa304d90cc657b19449e20e28ffbbef222e044d6
Steffen Gebert <steffen.gebert@typo3.org>
2014-05-17 10:05:00 +0200

    Set varnish instance name

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ default[:varnish][:vcl_source] = 'vbg9.vcl.erb'
 default[:varnish][:vcl_cookbook] = 'site-vbg9'
 default[:varnish][:backend_host] = 'www.volksbegehren-g9.de'
 default[:varnish][:backend_port] = 80
+default[:varnish][:instance] = 'vbg9'
